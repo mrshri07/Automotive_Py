526e8c93c30c2eca38c366ceef334fac5799fe38
shri <058yrs@gmail.com>
2025-05-30 11:40:18 +0530

    errors resloved

--- Dashboard_Warning_Simulation.py
+++ Dashboard_Warning_Simulation.py
@@ -1,26 +1,34 @@
 import random
+import time
 
 
 def dashboard_Warning():
 
-    try:
         # giving the ramdom realistic values to the parameters
-        while true:
+        while True:
             # fuel level indicator
             fuel_level = random.randint(0, 100)
             #engine temoerature
             Engine_temp = random.randint(70, 120)
             #oil pressure
-            oil_pressure = random.uniform(50, 100)
+            oil_pressure = random.uniform(0.5, 5.0)
 
             if  80 < fuel_level <= 100:
                 print(f"the fuel level is reached to = {fuel_level}")
+            if Engine_temp > 100:
+                print(f"the engine temperature is reached to ={Engine_temp} C")
+            if oil_pressure > 80:
+                print(f"the oil pressure is reached to {oil_pressure:.2f} ")
+
+            time.sleep(2)
 
 
+    try:
+        dashboard_Warning()
     except KeyboardInterrupt:
         print("warning is done ")
 
 
-dashboard_Warning()
+
 
 
